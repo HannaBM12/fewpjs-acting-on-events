9700193b21d61aa2c3b1d4a806fa92bca5347507
HannaBM12 <Hannabmulugeta@gmail.com>
2021-03-27 14:04:49 -0400

    commit tree 76209d0bbdd313d7a2b82f6307476a8f862cec0e

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Your code here
+
+function moveDodgerLeft(){
+    let leftNumbers = dodger.style.left.replace('px', " ")
+    let left = parseInt(leftNumbers, 10)
+    
+    if (left > 0){
+        dodger.style.left = `${left - 1}px`
+    }
+    
+}
+
+
+function moveDodgerRight(){
+    let rightNumber = dodger.style.left.replace("px", " ")
+    let right = parseInt(rightNumber, 10)
+    
+    if (right<359){
+        dodger.style.left = `${right + 1}px`
+    }
+}
